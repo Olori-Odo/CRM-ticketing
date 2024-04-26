55074ccf83806fb4eb8b37dd069fabbf23d1c0a9
Fatai Akinyemi <ojuotimi04@gmail.com>
2024-04-26 16:24:50 +0100

    router

--- src/router.jsx
+++ src/router.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import TicketForm from "./ticketForm/ticketForm";
 import DashboardLayout from "./Layout/DashboardLayout";
-import NewTicket from "./newTicket/newTicket";
+import NewTicket from "./newTicket/NewTicket";
 import Login from "./LoginForm/Login";
 import SignUp from "./Signup/Signup";
 import Users from "./Users";
