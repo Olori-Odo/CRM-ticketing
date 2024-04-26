864823b4010c54dc1f539ed4a634b6fdf3c68a71
Fatai Akinyemi <ojuotimi04@gmail.com>
2024-04-26 16:08:44 +0100

    avatar update

--- src/officials/Officials.jsx
+++ src/officials/Officials.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import AFO from "../assets/images/AFO.png";
+import Rahmat from "../assets/images/Rahmat.jpeg";
+import Hafsah from "../assets/images/Hafsah.jpeg";
 import img from "../assets/images/avatar.png";
 import "./Officials.css";
 
@@ -6,42 +9,42 @@ const Officials = () => {
   return (
     <section id="officials-wrap">
       <div>
-        <img src={img} alt="" className="img" />
+        <img id="avatar" src={AFO} alt="" className="img" />
         <h3>FATAI Akinyemi</h3>
       </div>
 
       <div>
-        <img src={img} alt="" className="img" />
+        <img id="avatar" src={img} alt="" className="img" />
         <h3>RAPHAEL Ojo</h3>
       </div>
 
       <div>
-        <img src={img} alt="" className="img" />
+        <img id="avatar" src={Hafsah} alt="" className="img" />
         <h3>HAFSAH Fashola</h3>
       </div>
 
       <div>
-        <img src={img} alt="" className="img" />
+        <img id="avatar" src={img} alt="" className="img" />
         <h3>LATEEF Salako</h3>
       </div>
 
       <div>
-        <img src={img} alt="" className="img" />
+        <img id="avatar" src={img} alt="" className="img" />
         <h3>ABDULLAHI Ilesanmi</h3>
       </div>
 
       <div>
-        <img src={img} alt="" className="img" />
+        <img id="avatar" src={Rahmat} alt="" className="img" />
         <h3>RAHMATALLAH AbdulRaheem</h3>
       </div>
 
       <div>
-        <img src={img} alt="" className="img" />
+        <img id="avatar" src={img} alt="" className="img" />
         <h3>OLAWALE Obaweya</h3>
       </div>
 
       <div>
-        <img src={img} alt="" className="img" />
+        <img id="avatar" src={img} alt="" className="img" />
         <h3>JAMES Nwali</h3>
       </div>
     </section>
